1f9d6296e803560417bf570037fcf342ad89990b
FPierre <pflauder@gmail.com>
2014-06-10 22:32:36 +0200

    Regions file

---
+++ utils/data/regions.php
@@ -0,0 +1,27 @@
+<?php
+
+$regions = array(
+    '82' => 'Rhône-Alpes',
+    '22' => 'Picardie',
+    '83' => 'Auvergne',
+    '93' => 'Provence-Alpes-Côte d\'Azur',
+    '21' => 'Champagne-Ardenne',
+    '73' => 'Midi-Pyrénées',
+    '91' => 'Languedoc-Roussillon',
+    '25' => 'Basse-Normandie',
+    '54' => 'Poitou-Charentes',
+    '24' => 'Centre',
+    '74' => 'Limousin',
+    '2A' => 'Corse',
+    '21' => 'Bourgogne',
+    '53' => 'Bretagne',
+    '72' => 'Aquitaine',
+    '43' => 'Franche-Comté',
+    '23' => 'Haute-Normandie',
+    '52' => 'Pays de la Loire',
+    '31' => 'Nord-Pas-de-Calais',
+    '42' => 'Alsace',
+    '11' => 'Île-de-France',
+);
+
+?>
