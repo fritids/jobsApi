d1084a9fe2fcf10aa9644541173faa7e5cd81b3b
FPierre <pflauder@gmail.com>
2014-06-23 21:59:01 +0200

    Some testsé é

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
@@ -13,10 +13,35 @@
     <link rel="stylesheet/less" href="assets/less/styles.less" />
 </head>
 <body>
-    <!-- View section -->
-    <section>
-        TEST
-    </section>
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">JobsAPI</a>
+            </div>
+        
+            <div class="navbar-collapse collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#">Rapport</a></li>
+                    <li><a href="#">Statistiques</a></li>
+                    <li><a href="#">Paramètres</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid">
+        <div class="row">
+            <h2 class="page-header">Rapport</h2>
+    
+            <div class="col-md-5 col-md-offset-1">
+                <h4>Alsacréations</h4>
+            </div>
+
+            <div class="col-md-5">
+                <h4>Site 2</h4>
+            </div>
+        </div>
+    </div>
 
     <!-- jQuery -->
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
