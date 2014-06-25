6c7c36c268b0a2ac3365056860814210f3775f54
FPierre <pflauder@gmail.com>
2014-06-25 20:00:20 +0200

    Beginning of Monitor class

--- Api/Feeder.php
+++ Api/Feeder.php
@@ -12,37 +12,6 @@ class Feeder {
     }
 
     public function indexElement($index, $type, $element, $id = NULL) {
-        // echo print_r($element, TRUE); // exit(0);
-
-        /*foreach ($element as $e) {
-            echo gettype($e) . ' => ' . print_r($e, TRUE) . "\n";
-        }*/
-
-        /*$element = array(
-            'index' => 'jobsapi',
-            'type'  => 'job',
-            'body'  => array(
-                'websiteName' => 'Alsacréations',
-                'websiteUrl'  => 'http://emploi.alsacreations.com',
-                'jobTitle'    => 'Intégrateur-développeur web mobile',
-                'jobUrl'      => 'http://emploi.alsacreations.com/offre-554465-Integrateur-developpeur-web-mobile.html',
-                'companyName' => 'L2lconsulting',
-                'companyUrl'  => '',
-                'publicationDate' => '25/06/2014',
-                'recoveryDate'    => '25/06/2014',
-                'jobCityName'     => 'Bouy-Luxembourg',
-                'jobPostalCode'   => '10220',
-                'jobType'         => 'CDI',
-                'jobRegionName'   => '',
-                'requiredSkills'  => array(),
-                'jobLocation'     => array(
-                    0 => 48.3833,
-                    1 => 4.26667
-                )
-            ),
-            'id' => 'b699ea876f57f168e7552289886b16a0'
-        );*/
-
         $params = array(
             'index' => $index,
             'type'  => $type,
--- Monitoring/Monitor.php
+++ Monitoring/Monitor.php
@@ -2,9 +2,35 @@
 
 namespace Monitoring;
 
+require 'Api/Feeder.php';
+require 'Api/Utils.php';
+
+use Api\Feeder;
+use Api\Utils;
+
 class Monitor implements \SplObserver {
+    public function __construct() {
+        $this->feeder = new Feeder('localhost', 9200);
+    }
+
     public function update(SplSubject $subject) {
         return error_log($subject->exception);
+    }
+
+    public function writeError() {
+        $status = $this->feeder->indexElement('jobsapi', 'errors', $this->data, $this->id);
+    }
+
+    public function readError() {
+
+    }
+
+    public function getAllErrors() {
+
+    }
+
+    public function deleteError() {
+
     }
 }
 
